dfd8e54dfda966542f98bfc9fa9d18d83017486c
unknown <madaawadh1@gmail.com>
2024-10-02 08:37:12 +0300

    added scroll

--- src/components/amy.jsx
+++ src/components/amy.jsx
@@ -2,17 +2,30 @@ import { useState } from 'react'
 import Comments from './Comments'
 function amy(props) {
     const [text1, setText1] = useState("")
+    const chatBox1 = document.getElementById('chatBox1');
   
+   
+    function send2(){props.fun([...props.chat,[1,text1,props.name]]);
+      
+      setTimeout(scrll, 500)
+      console.log(chatBox.scrollHeight)
+// Function to scroll the chat box to the bottom
+
+}
+function scrll(){
+    chatBox1.scrollTop = chatBox1.scrollHeight+40
+    chatBox.scrollTop = chatBox.scrollHeight+40
+ }
     return (
         <div style={{width:"260px",backgroundImage:"url(https://i.ibb.co/3cQ6swn/pngwing-com-1.png)",backgroundSize:"contain",backgroundPosition:"center", backgroundRepeat:"no-repeat"}} className="h-[400px] m-5 py-12 pl-[45px] pr-[45px]" >
             <div className='w-[100%] h-[100%] bg-[#FAF6F0]'> 
-            <div className="h-[80%] w-5/5 overflow-auto " style={{scrollbarWidth: "none"}}>
+            <div className="h-[80%] w-5/5 overflow-auto mb-5" id='chatBox1' style={{scrollbarWidth: "none"}}>
         {props.chat.length>=1?props.chat.map((el)=><Comments cmntId={el[0]} comment={el[1]} uId={1} name={el[2]}/>):""}
         </div>
         <label className="input input-bordered flex items-center p-1">
            
            <textarea type="text" placeholder='Send Text' className="input  w-4/4 grow h-[100%] p-0" onChange={(e)=>e.target.value!=""?setText1(e.target.value):""} />
-           <img  src="https://i.ibb.co/sHVJ3B1/pngaaa-com-7708063-2.png" className="w-5" alt="" srcset="" onClick={()=>props.fun([...props.chat,[1,text1,props.name]])}/>
+           <img  src="https://i.ibb.co/sHVJ3B1/pngaaa-com-7708063-2.png" className="w-5" alt="" srcset="" onClick={send2}/>
          </label>
    </div>
    </div>
--- src/components/jhon.jsx
+++ src/components/jhon.jsx
@@ -3,29 +3,38 @@ import Comments from './Comments'
 
 function jhon(props) {
     const [text, setText] = useState("")
+    
     const chatBox = document.getElementById('chatBox');
+    
 
+    function send(){props.fun([...props.chat,[2,text,props.name]]);
+      
+      setTimeout(scrll, 500)
+      console.log(chatBox.scrollHeight)
 // Function to scroll the chat box to the bottom
-function scrollToBottom() {
-chatBox.scrollTop = chatBox.scrollHeight;
-window.onload = scrollToBottom;
+
+}
+function scrll(){
+   chatBox.scrollTop = chatBox.scrollHeight+40
+   chatBox1.scrollTop = chatBox1.scrollHeight+40
 }
     return (
       
       <div style={{width:"260px",backgroundImage:"url(https://i.ibb.co/3cQ6swn/pngwing-com-1.png)",backgroundSize:"contain",backgroundPosition:"center", backgroundRepeat:"no-repeat"}} className="h-[400px] m-5 py-12  pl-[45px] pr-[45px] " >
             <div className='w-[100%] h-[100%] bg-[#FAF6F0]'> 
-            <div className="h-[80%] w-5/5 overflow-auto " id='chatBox' style={{scrollbarWidth: "none"}}>
+            <div className="h-[80%] w-5/5 overflow-auto mb-5" id='chatBox' style={{scrollbarWidth: "none"}}>
             
   {props.chat.length>=1?props.chat.map((el)=><Comments cmntId={el[0]} comment={el[1]} uId={2} name={el[2]} />):""}
           
            
             </div>
-            <label className="input input-bordered flex items-center p-1">
+          
+            <label className="input input-bordered flex items-center p-1 ">
            
-  <textarea type="text" placeholder='Send Text' className="input  w-4/4 grow h-[100%] p-0" onChange={(e)=>e.target.value!=""?setText(e.target.value):""} />
-  <img onClick={()=>props.fun([...props.chat,[2,text,props.name]])} src="https://i.ibb.co/sHVJ3B1/pngaaa-com-7708063-2.png" className="w-5" alt="" srcset="" />
+  <textarea type="text" placeholder='Send Text' className="input  w-4/4 grow h-[100%] p-0 " onChange={(e)=>e.target.value!=""?setText(e.target.value):""} />
+  <img onClick={send} src="https://i.ibb.co/sHVJ3B1/pngaaa-com-7708063-2.png" className="w-5" alt="" srcset="" />
 </label>
-         {/* <input type="text"  />  */}
+        
          
        </div>
        </div>
